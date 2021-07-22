4844b227c8cddfd26db58cf3f6de15cdf69dfdd8
Julian Hansen <73107104+Cryptonian007@users.noreply.github.com>
2021-07-22 15:18:50 +0530

    Update add.py

--- add.py
+++ add.py
@@ -55,7 +55,7 @@ def banner():
     for char in b:
         print(f'{random.choice(colors)}{char}{rs}')
     #print('=============SON OF GENISYS==============')
-    print(f'{lg}   Version: {w}1.0{lg} | Author: {w}Cryptonian{rs}\n')
+    print(f'{lg}   Version: {w}1.2{lg} | Author: {w}Cryptonian{rs}\n')
 
 
 # function to clear screen
